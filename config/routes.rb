5fc7af4577f77c60020fa0b9f42ac4644de97380
小若憲史 <kenjikowaka@kowakakenjinoMacBook-Pro.local>
2023-04-24 21:53:29 +0900

    device 導入　画像登録　アカウント画面登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'accounts/show'
+  devise_for :users
+  resources :posts
+  root "posts#index"
+  resources :users
+  resource :account, only: [:edit, :update,:show]
+  get '/account', to: 'accounts#show'
+  get '/users', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
